9c721f8f2d17f4934d32e091c15a0f63045c2bce
Yusuke Ando <yandod@gmail.com>
2017-03-13 01:16:18 +0900

    update logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,12 @@
 class HomeController < ApplicationController
   def index
-     @number = params[:q].to_i
-     if @number == 0 then
-       @number = 10
-     end
+    @number = params[:q].to_i
+    @brand = params[:b]
+    if @number == 0 then
+     @number = 10
+    end
+    if @brand == nil then
+      @brand = "domino"
+    end
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,19 +1,31 @@
 module HomeHelper
-  def hoge(number)
+  def hoge(number = 10, brand = "domino")
+    case brand
+    when "domino"
+      base_price = 2550
+      base_person = 3.06
+    when "pizzala"
+      base_price = 2991
+      base_person = 3.65
+    when "pizzahut"
+      base_price = 2500
+      base_person = 2.70
+    end
     beer = (number * 1.5) .ceil
-    pizza = (number / 3.65) .ceil
+    pizza = (number / base_person) .ceil
     subtotal_beer = beer * 200
-    subtotal_pizza = pizza * 2500
-    total = subtotal_beer + subtotal_pizza
+    subtotal_pizza = (pizza * base_price * 1.08).ceil
+    total = (subtotal_beer + subtotal_pizza)
     price = total / number
-    
+
     {
       :beer => beer,
       :pizza => pizza,
       :subtotal_beer => subtotal_beer,
       :subtotal_pizza => subtotal_pizza,
       :total => total,
-      :price => price
+      :price => price,
+      :base_price => base_price
     }
   end
 end
